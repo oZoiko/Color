694e3d89239cf54365560a3f9d68170e743ee698
Emanuel-Oliveira <101813364+oZoiko@users.noreply.github.com>
2022-05-09 18:55:42 -0300

    Responsive navbar.
    The navigation bar is now responsive and works on mobile devices with no issues.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//Toggle mobile navigation bar
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
